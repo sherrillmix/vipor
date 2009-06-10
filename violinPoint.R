87cb1ae8f4b40a253bb24e6444ac8602e91b3b77
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2009-06-10 00:14:30 +0000

    Fixing up levenshtein append prepend substring
    git-svn-id: https://microb98.med.upenn.edu/svn/scott/scripts/R@190 acd2042c-187b-11de-8279-d8d34a11d372

--- violinPoint.R
+++ violinPoint.R
@@ -1,19 +1,24 @@
 ##File Name: violinPoint.R
 ##Creation Date: Jun 04, 2009
-##Last Modified: Fri 05 Jun 2009 10:12:13 AM EDT
+##Last Modified: Fri 05 Jun 2009 07:28:16 PM EDT
 ##Created By: scott
 ##Summary: Function to take a vector of points and calculate an adjustment to x so they can be plotted cleanly
 
-require(fOptions)
-spreadY<-function(y,x=rep(1,length(y)),maxOffset=.2,stepNum=100,compare=FALSE,...){
-#thisNum<-20;groups<-6;x<-rep(1:groups,each=thisNum);y<-rnorm(thisNum*groups,rep(rnorm(groups),each=thisNum));jitter<-rep(NA,thisNum*groups);for(i in 1:groups){jitter[x==i]<-spreadY(y[x==i],maxOffset=.4)};plot(x+jitter,y)
+#spreadY: function to spread out points in a group for plotting based on kernel density
+#y: data points
+#x: groupings that will be jiggered
+#maxOffset: maximum amount to spread (usually not reached)
+#stepNum: number of breaks to use in density estimation
+#compare: use normal runif for comparison to low discrepancy 
+#...: additional arguments to density
+spreadY<-function(y,x=rep(1,length(y)),maxOffset=.4,stepNum=100,compare=FALSE,...){
+	#thisNum<-20;groups<-6;x<-rep(1:groups,each=thisNum);y<-rnorm(thisNum*groups,rep(rnorm(groups),each=thisNum))+rep(c(0,2),length.out=groups*thisNum);plot(x+spreadY(y,x,maxOffset=.8),y)
 	output<-rep(NA,length(y))
-	require(fOptions)
 	#low discrepancy "random"
 	for(j in unique(x)){
 		dense<-density(y,n=stepNum,...)
 		groupSelector<-x==j
-		if(!compare) randomJitter<-runif.halton(sum(groupSelector),1)[rank(y[groupSelector])]
+		if(!compare) randomJitter<-vanDerCorput(sum(groupSelector))[rank(y[groupSelector])]
 		else randomJitter<-runif(sum(groupSelector),0,1)
 		for(i in 1:stepNum){
 			selector<-y[groupSelector]<=dense$x[i]&is.na(output[groupSelector])
@@ -22,3 +27,37 @@ spreadY<-function(y,x=rep(1,length(y)),maxOffset=.2,stepNum=100,compare=FALSE,..
 	}
 	return(output)
 }
+
+
+
+
+
+#helper functions from https://stat.ethz.ch/pipermail/r-help/2008-May/162911.html
+number2digits=function(n,base){
+  #first digit in output is the least significant
+  digit<-n%%base
+  if (n<base)
+    return(digit)
+  else
+    return(c(digit,number2digits((n-digit)/base,base)))
+}
+
+digits2number=function(digits,base){
+  #first digit in input should be the most significant
+  output<-0
+  for (digit in digits) output<-(base*output)+digit
+  return(output)
+}
+
+vanDerCorput=function(n,base=2){
+  #generate n first digits of the van der Corput sequence
+  output=rep(NA,n)
+  for(i in 1:n){
+    digits<-number2digits(i,base)
+    output[i]<-digits2number(digits,base)/base^length(digits)
+  }
+  return(output)
+}
+
+
+
